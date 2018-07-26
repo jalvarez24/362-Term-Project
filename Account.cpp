7b10f518a86d0c6c0940ca7460ff8eea5f48d696
Danny Pham <dannypham92@hotmail.com>
2018-07-26 10:45:14 -0700

    Add files via upload

---
+++ Account.cpp
@@ -0,0 +1,95 @@
+#include "Account.h"
+
+int Account::id = 1;
+
+Account::Account()
+{
+	accountID = id;
+	id++;
+}
+
+Account::Account(string _username, string _pw, string _name, string _ccn, string _cce, string _addr, string _email, string _dob, string _home, string _cell, char _sex)
+{
+	accountID = id;
+	id++;
+
+	userName = _username;
+	password = _pw;
+	accountHolderName = _name;
+	creditCardNum = _ccn;
+	creditCardExpiration = _cce;
+	billingAddress = _addr;
+	email = _email;
+	dateOfBirth = _dob;
+	homePhone = _home;
+	cellPhone = _cell;
+	sex = _sex;
+}
+
+
+Account::~Account()
+{
+}
+
+void Account::setAccountHolderName(string _name)
+{
+	accountHolderName = _name;
+}
+
+void Account::setPassword(string _pw)
+{
+	password = _pw;
+}
+
+void Account::setCreditCardNum(string _num)
+{
+	creditCardNum = _num;
+}
+
+void Account::setCreditCardExpiration(string _exp)
+{
+	creditCardExpiration = _exp;
+}
+
+void Account::setBillingAddress(string _addr)
+{
+	billingAddress = _addr;
+}
+
+void Account::setEmail(string _email)
+{
+	email = _email;
+}
+
+void Account::setDateOfBirth(string _dob)
+{
+	dateOfBirth = _dob;
+}
+
+void Account::setHomePhone(string _home)
+{
+	homePhone = _home;
+}
+
+void Account::setCellPone(string _cell)
+{
+	cellPhone = _cell;
+}
+
+int Account::getID() const
+{
+	return accountID;
+}
+
+string Account::getName() const
+{
+	return accountHolderName;
+}
+
+void Account::print() const
+{
+	cout << "Hello, " << accountHolderName << "!\tID: " << accountID << "\n";
+	cout << "Addr: " << billingAddress << endl;
+	cout << "Home Phone: " << homePhone << endl;
+	cout << "Cell Phone: " << cellPhone << endl;
+}
---
+++ Account.h
@@ -0,0 +1,51 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Account
+{
+public:
+	Account();
+	Account(string, string, string, string, string, string, string, string, string, string, char);
+
+	~Account();
+
+	void setAccountHolderName(string);
+	void setPassword(string);
+	void setCreditCardNum(string);
+	void setCreditCardExpiration(string);
+	void setBillingAddress(string);
+	void setEmail(string);
+	void setDateOfBirth(string);
+	void setHomePhone(string);
+	void setCellPone(string);
+	
+	int getID() const;
+	string getName() const;
+
+	void print() const;
+
+
+
+
+private:
+	int accountID;
+	string userName;
+	string password;
+	string accountHolderName;
+	string creditCardNum;
+	string creditCardExpiration;
+	string billingAddress;
+	string email;
+	string dateOfBirth;
+	string homePhone;
+	string cellPhone;
+	char sex;
+
+	static int id;
+};
+
+#endif
