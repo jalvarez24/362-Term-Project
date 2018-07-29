f2c65b5f88eca9a835a39bb244d5a47847f4623b
jalvarez24 <jayroalvarez@csu.fullerton.edu>
2018-07-28 19:05:59 -0700

    Added bookingID variable to Booking Class

--- Booking.cpp
+++ Booking.cpp
@@ -4,22 +4,25 @@ Booking class holds:
 */
 #include "Booking.h"
 
-Booking::Booking() 
-{
+int Booking::id = 1;
 
-}
+Booking::Booking(){}
 
-Booking::~Booking()
-{
-
-}
+Booking::~Booking(){}
 
 Booking::Booking(string flightNum, int accountNum, int seat, double _price)
 {
+	bookingID = id;
+	id++;
 	flightNumber = flightNum;
 	accountNumber = accountNum;
 	assignedSeat = seat;
 	price = _price;
+}
+
+int Booking::getBookingID()
+{
+	return bookingID;
 }
 
 string Booking::getFlightNumber()
