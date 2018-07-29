464201bf7d5c9a5afdb6bb6f0fca6aad4fc40cdf
jalvarez24 <jayroalvarez@csu.fullerton.edu>
2018-07-28 19:16:01 -0700

    Added price to Flight Class

--- Flight.h
+++ Flight.h
@@ -5,32 +5,62 @@
 #include <string>
 using namespace std;
 
-class Flight
-{
-public:
-	Flight();
-	~Flight();
-	Flight(string, string, string, int, int, int, int, int, int, int, int, char[]);
-	string getFlightNumber();
-	int getDepartureTime();
-	int getArrivalTime();
-	string getDepartureLocation();
-	string getArrivalLocation();
-	void print();
-	void displaySeats();
-private:
-	string flightNumber;
-	string toLocation;
-	string fromLocation;
-	int departureMonth;
-	int departureDay;
-	int departureYear;
-	int departureTime;
-	int arrivalMonth;
-	int arrivalDay;
-	int arrivalYear;
-	int arrivalTime;
-	char seats[72];
+class Flight
+
+{
+
+public:
+
+	Flight();
+
+	~Flight();
+
+	Flight(string, string, string, int, int, int, int, int, int, int, int, char[], double);
+
+	string getFlightNumber();
+
+	int getDepartureTime();
+
+	int getArrivalTime();
+
+	string getDepartureLocation();
+
+	string getArrivalLocation();
+
+	void print();
+
+	void displaySeats();
+	
+	double getprice();
+
+private:
+
+	string flightNumber;
+
+	string toLocation;
+
+	string fromLocation;
+
+	int departureMonth;
+
+	int departureDay;
+
+	int departureYear;
+
+	int departureTime;
+
+	int arrivalMonth;
+
+	int arrivalDay;
+
+	int arrivalYear;
+
+	int arrivalTime;
+
+	char seats[72];
+	
+	double price;
+
 };
 
 #endif
